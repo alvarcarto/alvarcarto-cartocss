386868759dc6e10614a9b555c13bfcc6d04c275b
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2017-06-13 21:57:23 +0300

    Update style names

--- tools/build.sh
+++ tools/build.sh
@@ -8,8 +8,8 @@ rm -f dist/*.xml
 carto styles/black/project.mml > dist/black.xml
 carto styles/bw/project.mml > dist/bw.xml
 carto styles/copper/project.mml > dist/copper.xml
-carto styles/cotton/project.mml > dist/cotton.xml
+carto styles/marshmellow/project.mml > dist/marshmellow.xml
 carto styles/gray/project.mml > dist/gray.xml
-carto styles/pastel-blue/project.mml > dist/pastel-blue.xml
-carto styles/pastel-green/project.mml > dist/pastel-green.xml
+carto styles/iceberg/project.mml > dist/iceberg.xml
+carto styles/madang/project.mml > dist/madang.xml
 carto styles/petrol/project.mml > dist/petrol.xml
