e8fdf7b3ed18ea8f90058ed33ccc61cf6923e797
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2017-06-23 15:07:39 +0300

    Make madang green a bit darker and fix deploy script

--- styles/madang/style.mss
+++ styles/madang/style.mss
@@ -3,7 +3,7 @@ Map {
 }
 
 @water-color: #fff;
-@land-color: #BDECB6;
+@land-color: #a7e19e;
 
 @standard-halo-radius: 1;
 @standard-halo-fill: rgba(255,255,255,0.6);
--- tools/deploy.sh
+++ tools/deploy.sh
@@ -5,8 +5,8 @@ set -x
 
 npm run build
 
-ssh alvar@$SERVER_HOST "bash -l -c \"rm ~/mapnik-styles/*.xml\""
+ssh alvar@$SERVER_HOST "bash -l -c \"rm -f ~/mapnik-styles/*.xml\""
 scp -r dist/*.xml alvar@$SERVER_HOST:~/mapnik-styles
 
-ssh alvar@$SERVER_HOST "bash -l -c \"rm -r /mnt/volume1/alvar/tiles/*\""
+ssh alvar@$SERVER_HOST "bash -l -c \"rm -rf /mnt/volume1/alvar/tiles/*\""
 ssh alvar@$SERVER_HOST "bash -l -c 'PATH=$PATH:/home/alvar/.nvm/versions/node/v6.9.4/bin pm2 restart all'"
