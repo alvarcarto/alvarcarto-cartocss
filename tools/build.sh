f13f70336b0a80a896f037a6da1b6dd175eed27b
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2019-09-03 14:05:24 +0300

    Add contrast-black-london

--- tools/build.sh
+++ tools/build.sh
@@ -17,6 +17,7 @@ carto styles/sunset/project.mml > dist/sunset.xml
 carto styles/contrast-black/project.mml > dist/contrast-black.xml
 carto styles/contrast-black-4x-roads/project.mml > dist/contrast-black-4x-roads.xml
 carto styles/contrast-black-2x-roads/project.mml > dist/contrast-black-2x-roads.xml
+carto styles/contrast-black-london/project.mml > dist/contrast-black-london.xml
 
 carto styles/bg-black/project.mml > dist/bg-black.xml
 carto styles/bg-gray/project.mml > dist/bg-gray.xml
