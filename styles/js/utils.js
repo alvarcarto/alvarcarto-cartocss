27a7f2cb9d62529879d78a819a90ce5a6c2f3215
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2020-04-14 19:27:50 +0300

    add missing utility script

--- styles/js/utils.js
+++ styles/js/utils.js
@@ -1,14 +1,43 @@
 const _ = require('lodash');
+const chroma = require('chroma-js');
 
 const MIN_ZOOM = 0;
 const MAX_ZOOM = 16;
 
-function scaleBetween(unscaledNum, minAllowed, maxAllowed, min, max) {
-  return (maxAllowed - minAllowed) * (unscaledNum - min) / (max - min) + minAllowed;
+function scaleBetweenRanges(range1Val, range1Min, range1Max, range2Min, range2Max, easing)  {
+  // range1Val position from 0.0 - 1.0 in the range 1
+  const range1Pos = (range1Val - range1Min) / (range1Max - range1Min);
+
+  // Normalize in case value was out of range bounds
+  const normRange1Pos = Math.max(Math.min(range1Pos, 1), 0);
+  const easedPos = easing(normRange1Pos);
+
+  const range2Val = easedPos * (range2Max - range2Min) + range2Min;
+  return range2Val;
 }
 
-function scale(zoomLevel, min, max) {
-  return Number(scaleBetween(zoomLevel, min, max, MIN_ZOOM, MAX_ZOOM).toFixed(2));
+function scaleFromEasing(easing) {
+  return (zoomLevel, minVal, maxVal, opts = {}) => {
+    const newOpts = _.extend({}, opts, { easing });
+    return scale(zoomLevel, minVal, maxVal, newOpts);
+  }
+}
+
+function scale(zoomLevel, minVal, maxVal, _opts = {}) {
+  const opts = _.extend({
+    easing: val => val,
+  }, _opts);
+
+  if (_.isString(minVal) && _.isString(maxVal)) {
+    const scale = chroma.scale([minVal, maxVal]);
+
+    // Chroma scale takes value between 0.0 and 1.0
+    const scaleValue = scaleBetweenRanges(zoomLevel, MIN_ZOOM, MAX_ZOOM, 0, 1, opts.easing);
+    return scale(scaleValue).hex();
+  }
+
+  const value = scaleBetweenRanges(zoomLevel, MIN_ZOOM, MAX_ZOOM, minVal, maxVal, opts.easing);
+  return Number(value.toFixed(2));
 }
 
 function indentExceptFirst(str, level) {
@@ -30,8 +59,9 @@ function indent(str, level) {
 }
 
 module.exports = {
-  scaleBetween,
+  scaleBetweenRanges,
   scale,
+  scaleFromEasing,
   indent,
   indentExceptFirst,
   MIN_ZOOM,
